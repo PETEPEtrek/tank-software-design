c8c6e4d449dafeb030b87f676dbfb33a6e98184d
PETEPEtrek <senatoroff.p@gmail.com>
2024-10-06 13:57:50 +0300

    hometask-3

--- src/test/java/ru/mipt/bit/platformer/abstractions/TankTest.java
+++ src/test/java/ru/mipt/bit/platformer/abstractions/TankTest.java
@@ -11,7 +11,7 @@ public class TankTest {
     @Test
     public void tankRotateMoveUpNoObstacle() {
 
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         tank.moveUp(new GridPoint2(2, 2), Direction.UP);
 
         assertEquals(new GridPoint2(1, 1), tank.getPlayerDestinationCoordinates());
@@ -20,7 +20,7 @@ public class TankTest {
 
     @Test
     public void tankRotateNotMoveUpObstacle() {
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         Tree tree = new Tree(new GridPoint2(1, 1), 0f);
         tank.moveUp(tree.getCoordinates(), Direction.UP);
 
@@ -31,7 +31,7 @@ public class TankTest {
     @Test
     public void tankRotateMoveRightNoObstacle() {
 
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         tank.moveRight(new GridPoint2(2, 2), Direction.RIGHT);
 
         assertEquals(new GridPoint2(2, 0), tank.getPlayerDestinationCoordinates());
@@ -41,7 +41,7 @@ public class TankTest {
     @Test
     public void tankRotateNotMoveRightObstacle() {
 
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         Tree tree = new Tree(new GridPoint2(2, 0), 0f);
         tank.moveRight(tree.getCoordinates(), Direction.RIGHT);
 
@@ -51,7 +51,7 @@ public class TankTest {
 
     @Test
     public void tankRotateMoveDownNoObstacle() {
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         tank.moveDown(new GridPoint2(2, 2), Direction.DOWN);
 
         assertEquals(new GridPoint2(1, -1), tank.getPlayerDestinationCoordinates());
@@ -61,7 +61,7 @@ public class TankTest {
     @Test
     public void tankRotateNotMoveDownObstacle() {
 
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         Tree tree = new Tree(new GridPoint2(1, -1), 0f);
         tank.moveDown(tree.getCoordinates(), Direction.DOWN);
 
@@ -72,7 +72,7 @@ public class TankTest {
     @Test
     public void tankRotateMoveLeftNoObstacle() {
 
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         tank.moveLeft(new GridPoint2(2, 2), Direction.LEFT);
 
         assertEquals(new GridPoint2(0, 0), tank.getPlayerDestinationCoordinates());
@@ -82,7 +82,7 @@ public class TankTest {
     @Test
     public void tankRotateNotMoveLeftObstacle() {
 
-        Tank tank = new Tank(0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
+        Tank tank = new Tank(Direction.UP, 1f, 0.4f, new GridPoint2(1, 0), new GridPoint2(1, 0));
         Tree tree = new Tree(new GridPoint2(0, 0), 0f);
         tank.moveLeft(tree.getCoordinates(), Direction.LEFT);
 
